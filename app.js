75c6a909f3b71601815e6160df0f71316bc662c7
alexandrprokhorov1988 <alexandrprokhorov1988@gmail.com>
2020-10-16 13:31:41 +0300

    fix router

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use(cors({ origin: true }));
 app.use(function (req, res, next) {
 
   // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:8888');
+  res.setHeader('Access-Control-Allow-Origin', 'https://apro.students.nomoreparties.xyz/');
 
   // Request methods you wish to allow
   res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
