f32a265b718596233af43c63306299116bd420d7
alexandrprokhorov1988 <alexandrprokhorov1988@gmail.com>
2020-10-16 13:30:34 +0300

    fix router

--- controllers/login.js
+++ controllers/login.js
@@ -16,11 +16,10 @@ module.exports.login = (req, res, next) => {
         NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
         { expiresIn: '7d' },
       );
-      // res.send({ token });
       res.cookie('authorization', token, {
         maxAge: 3600000 * 24 * 7,
         httpOnly: true,
-        // sameSite: true,
+        sameSite: true,
       }).end(); // todo
     })
     .catch(next);
