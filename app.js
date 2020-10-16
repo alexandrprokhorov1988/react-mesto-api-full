f35198df67c2a010c771decb541dac6ad303b339
alexandrprokhorov1988 <alexandrprokhorov1988@gmail.com>
2020-10-16 13:32:36 +0300

    fix router

--- app.js
+++ app.js
@@ -31,27 +31,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
 });
 
-app.use(cors({ origin: true }));
-
-app.use(function (req, res, next) {
-
-  // Website you wish to allow to connect
-  res.setHeader('Access-Control-Allow-Origin', 'https://apro.students.nomoreparties.xyz/');
-
-  // Request methods you wish to allow
-  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
-
-  // Request headers you wish to allow
-  res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type');
-
-  // Set to true if you need the website to include cookies in the requests sent
-  // to the API (e.g. in case you use sessions)
-  res.setHeader('Access-Control-Allow-Credentials', true);
-
-  // Pass to next layer of middleware
-  next();
-});
-
+app.use(cors());
 app.use(cookieParser());
 app.use(limiter);
 app.use(helmet());
