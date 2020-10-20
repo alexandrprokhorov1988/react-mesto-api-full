49f35c69b076945974de3ca32d3f9832f210bdba
alexandrprokhorov1988 <alexandrprokhorov1988@gmail.com>
2020-10-20 14:14:21 +0300

    cors test

--- app.js
+++ app.js
@@ -31,14 +31,7 @@ mongoose.connect('mongodb://localhost:27017/mestodb', {
   useUnifiedTopology: true,
 });
 
-// app.use(cors({ credentials: true, origin: false }));
-app.use(function(req, res, next) {
-  res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
-  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-
-  next();
-});
+app.use(cors());
 app.use(cookieParser());
 app.use(limiter);
 app.use(helmet());
